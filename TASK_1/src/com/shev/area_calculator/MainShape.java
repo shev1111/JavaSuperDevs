b1ae41aa0e248d8670c45180ae80496d09639720
shev1111 <evhen.shev@gmail.com>
2018-09-13 19:43:59 +0300

    add MainShape class with logger

---
+++ TASK_1/src/com/shev/area_calculator/MainShape.java
@@ -0,0 +1,26 @@
+package com.shev.area_calculator;
+
+import com.shev.area_calculator.circle.Circle;
+import com.shev.area_calculator.parent_shape.Shape;
+import com.shev.area_calculator.square.Square;
+import com.shev.area_calculator.triangle.Triangle;
+
+import java.util.logging.Logger;
+
+public class MainShape {
+    private static Logger logger = Logger.getLogger(MainShape.class.getName());
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[3];
+        Triangle triangle = new Triangle (20,30,20);
+        shapes[0] = triangle;
+        Circle circle = new Circle(10);
+        shapes[1] = circle;
+        Square square = new Square(2, 3);
+        shapes[2] = square;
+        for(Shape shape:shapes) {
+            logger.info(shape.getClass()+" area = "+String.valueOf(shape.getArea()));
+        }
+
+    }
+}
